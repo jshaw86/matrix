8e485eb937472ef40e0f8f7a41d9a35fd4f58153
Jordan Shaw <jordanshaw86@earthlink.net>
2011-07-31 15:13:41 -0400

    fixed logic bug in best(), weighted down and right moves, backoff working, linked list through matrix working
    Signed-off-by: Jordan Shaw <jordanshaw86@earthlink.net>

--- shortest_path.cpp
+++ shortest_path.cpp
@@ -54,23 +54,32 @@ cell*** alloccells(int rows,int columns){
 
 cell* best(cell* up,cell* down,cell* left,cell* right){
 	cell* best =NULL;
-	if(!up->visited){
+	if(!down->visited && ( best == NULL || (best != NULL && down->weight < best->weight ))){
+			cout << "\t CHOOSING DOWN"<<endl;
+			best = down;
+
+	}
+
+	if(!right->visited && ( best == NULL || (best != NULL && right->weight <best->weight) )){
+			cout << "\t CHOOSING RIGHT"<<endl;
+			best = right;
+
+	}
+	
+	 
+	if(!up->visited && (best == NULL || (best != NULL && (up->weight * 3) < best->weight))){
+		cout << "\t CHOOSING UP"<<endl;
 		best = up;	
 
 	}	
 
-	if(!left->visited){
-		best = best == NULL || left->weight <up->weight?left:up;
+	if(!left->visited && (best == NULL || (best != NULL && (left->weight * 3) <best->weight))){
+			cout << "\t CHOOSING LEFT"<<endl;
+			best = left;
 
 	}
-	if(!down->visited){
-		best = best == NULL || down->weight < left->weight?down:left;
-
-	}
-	if(!right->visited){
-		best =  best == NULL || right->weight <down->weight?right:down;
-
-	}
+	
+	
 	
 	return best;
 	
@@ -133,6 +142,8 @@ cell* go(cell*** matrix,int x,int y,int rows,int columns){
 			cell* current = matrix[y][x];
 			current->visited = true;	
 
+			cout << "THE CURRENT IS:" << x << ","<<y <<"THE WEIGHT IS: "<< current->weight<<endl;
+
 			if(x ==(columns -1) && y == (rows -1)){
 			
 			//cout <<"BEST ID"<<best_cell<<endl;
@@ -147,94 +158,103 @@ cell* go(cell*** matrix,int x,int y,int rows,int columns){
 				cell* right = x!= columns -1? matrix[y][x+1]:edge_cell();
 
 				cell* best_cell = best(up,down,left,right);
-				current->best = best_cell;
-
-
+				int next_y = y+ y_direction(best_cell,up,down);
+				int next_x = x + x_direction(best_cell,left,right);
+				matrix[y][x]->best = matrix[next_y][next_x];
+				cout << "\tNEXT BEST IS: "<<best_cell<<":" << next_x <<"," << next_y<< "THE WEIGHT IS: " << matrix[next_y][next_x]->weight <<endl;
+				
 				if(best_cell == NULL){
-					cout << "BACKING OUT" <<endl;
+					cout << "\t\tBACKING OUT" <<endl;
 					return NULL;
 
 				}
 				else{
-					int next_y = y+ y_direction(best_cell,up,down);
-					int next_x = x + x_direction(best_cell,left,right);
-					//cout << "VISITED:" << matrix[y][x]->visited << endl;
-					//cout << "THE CURRENT IS:" << x << ","<<y <<"THE WEIGHT IS: "<< current->weight<<endl;
-					cout << "NEXT BEST IS: " << next_x <<"," << next_y<< "THE WEIGHT IS: " << matrix[next_y][next_x]->weight <<endl;
-					go(matrix,next_x,next_y,rows,columns);
-
-					return current->best;
 					
-		
-				/*current->best = go(matrix,next_x,next_y,rows,columns);
-				if(current->best != NULL){
-					return current->best;
-		
-				}
-				current->best = go(matrix,next_x,next_y,rows,columns);
-				if(current->best != NULL){
-					return current->best;
-		
-				}
-				current->best = go(matrix,next_x,next_y,rows,columns);
-				return current->best;
-				*/
+					//cout << "VISITED:" << matrix[y][x]->visited << endl;
+					cell* next =  go(matrix,next_x,next_y,rows,columns);
+					if(next != NULL){
+						return next;
+					}
+
+					next = go(matrix,next_x,next_y,rows,columns);
+					return next;
+					
+				
 			}
 		}
 }
 
+/*cell*** build_matrix(string input, int rows,int columns){
+	int** weights = new int*[columns];
+	*weights = new int[rows];
+	int row =0;
+	for(int i = 0;input.length();i++){
+		char c= input.at(i);
+		int column = 0;
+		if(&c == "|"){
+			int j = i;
+			while(j<input.length() && &input.at(j) != "|"){
+					j++;
+			}
+			column++;
+			weights[column][row] = input.substr(i,j);
+			if(column ==columns){
+				row++;
+				column = 0;
+	
+			}
+
+			i = j;
+
+		}
+
+	}
+
+	cell*** matrix = allocmatrix(rows,columns);
+	for(int x = 0;x <rows;x++){
+		for(int y =0;y<columns;y++){
+			alloccell(matrix,weights[y][x],x,y,rows,columns);		
+
+		}
+
+	}
+
+	return matrix;
+
+
+}
+*/
 int main(int argc,const char* argv[]){
 	int rows = 6;
 	int columns = 4;
 
 	srand ( time(NULL) );
 
-	cell*** matrix = alloccells(rows,columns);
 	
 	/*
 	 0       |       4       |       4       |       3       |
-	3       |       5       |       2       |       3       |
- 7       |       2       |       5       |       4       |
-5       |       9       |       6       |       7       |
-3       |       10      |       3       |       2       |
-8       |       10      |       2       |       0       |
+	 3       |       5       |       2       |       3       |
+   7       |       2       |       5       |       4       |
+   5       |       9       |       6       |       7       |
+   3       |       10      |       3       |       2       |
+   8       |       10      |       2       |       0       |
 	 */
 	/*
-	matrix[0][0]->weight = 3;
-	matrix[1][0]->weight = 8;
-	matrix[2][0]->weight = 4;
-	matrix[3][0]->weight = 4;
-	matrix[4][0]->weight = 5;
-	matrix[5][0]->weight = 4;
+	   0 | 5 | 7 | 6 |
+		 1 | 3 | 2 | 7 |
+		 9 | 2 | 1 | 6 |
+	  10 | 1 | 8 | 2 |
+		 2 | 3 | 6 | 10  |
+		 1 | 8 | 3 | 0 |
+		 */
 
-	matrix[0][1]->weight = 9;
-	matrix[1][1]->weight = 7;
-	matrix[2][1]->weight = 4;
-	matrix[3][1]->weight = 5;
-	matrix[4][1]->weight = 8;
-	matrix[5][1]->weight = 9;
-
-	matrix[0][2]->weight = 9;
-	matrix[1][2]->weight = 5;
-	matrix[2][2]->weight = 6;
-	matrix[3][2]->weight = 4;
-	matrix[4][2]->weight = 3;
-	matrix[5][2]->weight = 7;
-
-	matrix[0][3]->weight = 5;
-	matrix[1][3]->weight = 6;
-	matrix[2][3]->weight = 3;
-	matrix[3][3]->weight = 7;
-	matrix[4][3]->weight = 3;
-	matrix[5][3]->weight = 5;
-*/
+	cell*** matrix = alloccells(rows,columns);
+		
 	mprint(matrix,rows,columns);
 	cout << "------------traversing-------------"<<endl;
+	go(matrix,0,0,rows,columns);
 	int total = 0;
-	cell* HEAD = go(matrix,0,0,rows,columns);
-	
-	total = 0;
-	cell* current = HEAD;
+	cell* current = matrix[0][0];
 	while(current != NULL){
 		cout <<"SUMMING:"<< current->weight<<endl;
 		total += current->weight;	
